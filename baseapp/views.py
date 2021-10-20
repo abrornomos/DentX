c1874efeb87828a300341dc358df11238829ef83
Abror Mamataliev <abrornomos2018@gmail.com>
2021-10-20 11:51:54 +0500

    Adding board with waiting list and appointments list

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.conf import settings as global_settings
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from dentist.models import User as DentistUser
 
 # Create your views here.
+
+
 def board(request):
-    pass
+    if request.user.username not in request.session:
+        return redirect(f"{global_settings.LOGIN_URL}?next={request.path}")
+    user = User.objects.get(username=request.user.username)
+    dentist = DentistUser.objects.get(user=user)
+    return render(request, "baseapp/board.html", {
+        'dentist': dentist
+    })
